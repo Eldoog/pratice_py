9e30d3e42969e4f491a9f2ffe21d416660e3b2f6
Andy <a94qu0402@gmail.com>
2023-02-26 19:35:05 +0800

    基本操作 型別認識

---
+++ basic.py
@@ -0,0 +1,12 @@
+print("3" + "3")
+print(3 + 3)
+
+print(3 * "3")         #str * int 是合法 -> str 出現 int 次
+print("3" * 3)         #int * str 是合法 -> str 出現 int 次
+
+print(9)               #datatype -> int
+print('9')             #datatype -> string
+print("9")             #datatype -> string
+
+print(None)            #None 為一類別
+print(type(None))      #None Type
