819ce4fc76c13c93497bf7e57e6ee34f8490422a
Andy <a94qu0402@gmail.com>
2023-03-04 11:46:09 +0800

    range 範圍 + print 不自動換行

--- for.py
+++ for.py
@@ -1,3 +1,5 @@
-for i in range(1,10):
-    if i % 2 == 0:
-        print(i)
+for i in range(1,10):       #1~9
+    print(i, end="")        # print 會自動換行，加上 end=" " 可以不自動換行
+print("")
+for j in range(10):         #0~9
+    print(j, end="")
