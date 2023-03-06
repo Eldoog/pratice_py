a4fa783b5ff10df14ff3a493b7ccbe50327e7a0b
Andy <a94qu0402@gmail.com>
2023-03-06 10:58:22 +0800

    list 新增、插入

--- list.py
+++ list.py
@@ -3,14 +3,12 @@ list = [1,7,11,5]
 for i in list:          #此為印出 list 裡面的數值，非印出 list[i] 的 i
     print(i)
 
-
-def Max(l):
-    max = 0
-    for i in range(0,len(l)):
-        if(l[i]>max):
-            max = l[i]
-    return max
-print("Max:",Max(list))  
+# list 新增、插入
+print(list)
+list.append('15')
+print(list)
+list.insert(1,'2222')
+print(list)
 
 
 arr = ['偶數' , '奇數']
